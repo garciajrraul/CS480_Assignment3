2023f34591df384d4cdb2fd1013624f8d37f1d0c
garciajrraul <garciajr.raul@yahoo.com>
2022-03-16 17:32:01 -0700

    Update 1 to pagetable.c

--- pagetable.c
+++ pagetable.c
@@ -8,3 +8,16 @@ Name: 	Raul Garcia Jr
 */
 
 #include "main.h"
+#include "pagetable.h"
+
+unsigned int virtualToPageNum(unsigned int virtualAddress, unsigned int mask, unsigned int shift){
+
+}
+
+void pageInsert(PageTable *pagetable, unsigned int virtualAddress, unsigned int frame){
+
+}
+
+Map * pageLookup(PageTable *pageTable, unsigned int virtualAddress){
+
+}
